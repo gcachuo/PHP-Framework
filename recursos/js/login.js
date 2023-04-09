b21da863d01032f656a6f69236f234e03ec4b1f8
Memo Cachú <gcachu.o@gmail.com>
2023-04-09 15:56:43 -0600

    Fix ajax settings

--- recursos/js/login.js
+++ recursos/js/login.js
@@ -3,28 +3,20 @@
  */
 
 $(function () {
-    $("input").keypress(function (event) {
-        if (event.which === 13) {
-            event.preventDefault();
-            btnIniciarSesion();
-        }
-    });
+  $("input").keypress(function (event) {
+    if (event.which === 13) {
+      event.preventDefault();
+      btnIniciarSesion();
+    }
+  });
 });
 
 function btnIniciarSesion() {
-    $.ajaxSetup({
-        beforeSend: function (jqXHR, settings) {
-            if (settings.url.indexOf('http') === -1) {
-                settings.url = 'index.php/' + settings.url;
-            }
-        }
-    });
-    ajax('iniciarSesion', undefined, 'login');
+  ajax("iniciarSesion", undefined, "login");
 }
 
 function iniciarSesion(result) {
-    if (result.cambiarPass) {
-        aside("miperfil", "miperfil");
-    } else
-        navegar('inicio');
+  if (result.cambiarPass) {
+    aside("miperfil", "miperfil");
+  } else navegar("inicio");
 }
